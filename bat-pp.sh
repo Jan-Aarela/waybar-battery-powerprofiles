71a02e6e38d8fd883dc956febff7ea7dc8ca17c2
Jan Äärelä <132766267+Jan-Aarela@users.noreply.github.com>
2025-05-27 15:51:31 +0300

    Update bat-pp.sh

--- bat-pp.sh
+++ bat-pp.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+# This variable selects mode to run.
 MODE=$1
 
 # Power profile switcher
@@ -14,44 +15,49 @@ if [[ $MODE == "toggle" ]];then
     fi
 fi
 
-# Delay, so that powerprofile switches first.
-sleep 0.25
+# Refreshes the whole module.
+if [[ $MODE == "refresh" ]];then
 
-# Get battery information and power profile.
-BATTERY=$(upower -e | grep 'BAT')
-PERCENT=$(upower -i "$BATTERY" | awk '/percentage/ {print $2}' | tr -d '%')
-STATE=$(upower -i "$BATTERY" | awk '/state/ {print $2}' | tr -d '%')
-RATE=$(upower -i "$BATTERY" | awk '/energy-rate/ {print $2}' | tr -d '%')
-PROFILE=$(powerprofilesctl get)
+    # Delay, so that powerprofile switches first.
+    # Increase if doesn't update on click.
+    sleep 0.25
 
+    # Get battery information and power profile.
+    BATTERY=$(upower -e | grep 'BAT')
+    PERCENT=$(upower -i "$BATTERY" | awk '/percentage/ {print $2}' | tr -d '%')
+    STATE=$(upower -i "$BATTERY" | awk '/state/ {print $2}' | tr -d '%')
+    RATE=$(upower -i "$BATTERY" | awk '/energy-rate/ {print $2}' | tr -d '%')
+    PROFILE=$(powerprofilesctl get)
 
-# Case match profile.
-case "$PROFILE" in
-    performance) PROFILE=$" 󱀚"
-        ;;
-    balanced) PROFILE=$" "
-        ;;
-    power-saver) PROFILE=$" "
-        ;;
-esac
+    # Format profile icon.
+    # Nerd font used in this case.
+    case "$PROFILE" in
+        performance) PROFILE=$" 󱀚"
+            ;;
+        balanced) PROFILE=$" "
+            ;;
+        power-saver) PROFILE=$" "
+            ;;
+    esac
 
-# Set class.
-if [[ $STATE == "charging" ]];then
-    CLASS=$"charging"
-elif [[ $PERCENT -le 10  ]]; then
-    CLASS=$"critical"
-elif [[ $PERCENT -le 20 ]]; then
-    CLASS=$"warning"
-else
-    CLASS=$""
+    # Set class for styling.
+    if [[ $STATE == "charging" ]];then
+        CLASS=$"charging"
+    elif [[ $PERCENT -le 10  ]]; then
+        CLASS=$"critical"
+    elif [[ $PERCENT -le 20 ]]; then
+        CLASS=$"warning"
+    else
+        CLASS=$""
+    fi
+
+    # Set energy rate polarity.
+    if [[ $STATE == "charging" ]];then
+        TOOLTIP="+$RATE"
+    else
+        TOOLTIP=$"-$RATE"
+    fi
+
+    # Export as json.
+    printf '{"text": "%s", "class": "%s", "alt": "%s"}\n' "$PROFILE $PERCENT" "$CLASS" "$TOOLTIP"
 fi
-
-# Set energy rate polarity.
-if [[ $STATE == "charging" ]];then
-    TOOLTIP="+$RATE"
-else
-    TOOLTIP=$"-$RATE"
-fi
-
-# Export as json.
-printf '{"text": "%s", "class": "%s", "alt": "%s"}\n' "$PROFILE $PERCENT" "$CLASS" "$TOOLTIP"
