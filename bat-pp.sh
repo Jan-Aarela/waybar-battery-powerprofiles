511c9172e4586fc8675ced8b4a222305b1a4eb09
Jan Äärelä <132766267+Jan-Aarela@users.noreply.github.com>
2025-05-27 14:55:21 +0300

    Update bat-pp.sh

--- bat-pp.sh
+++ bat-pp.sh
@@ -1,10 +1,28 @@
 #/bin/bash
 
+MODE=$1
+
+# Power profile switcher
+if [[ $MODE == "toggle" ]];then
+    PROFILE=$(powerprofilesctl get)
+    if [[ $PROFILE == "performance" ]]; then
+        powerprofilesctl set balanced
+    elif [[ $PROFILE == "balanced" ]]; then
+        powerprofilesctl set power-saver
+    else
+        powerprofilesctl set performance
+    fi
+fi
+
+# Delay, so that powerprofile switches first.
+sleep 0.25
+
 # Get battery information and power profile.
 BATTERY=$(upower -e | grep 'BAT')
 PERCENT=$(upower -i "$BATTERY" | awk '/percentage/ {print $2}' | tr -d '%')
 STATE=$(upower -i "$BATTERY" | awk '/state/ {print $2}' | tr -d '%')
 PROFILE=$(powerprofilesctl get)
+
 
 # Case match profile.
 case "$PROFILE" in
